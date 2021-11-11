c30e0f1cee377dc9f4efbb6542643072e64d48b7
Barry A Dobyns <bdobyns@gmail.com>
2021-11-11 05:00:20 +0100

    fix logging to be more succinct, better handling of edge condtions

--- reboot-on-fail.sh
+++ reboot-on-fail.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 cd ~  
-TOOMANY=5
-MINHASHRATE=20
+TOOMANY=10 # it takes a few minutes to get everything stable after a reboot.
+MINHASHRATE=26 # at least one GPU running, even if not LHR mode
 HOWMANY_FILE=~/badhashes  
 CURRENT_HASHRATE=`curl -s localhost:4444 2>/dev/null | jq .Session.Performance_Summary | cut -f 1 -d .`
 HOWMANY=`cat $HOWMANY_FILE`  
 
 logmsg() {
-     echo -n $* " ">>${HOWMANY_FILE}.log
-     date >>${HOWMANY_FILE}.log
-     
-     echo -n $* " ">>/var/tmp/consoleSys.log
-     date >>/var/tmp/consoleSys.log
+     NOW=$( date '+%Y-%m-%d %H:%M:%S' )
+     echo $NOW $* " ">>${HOWMANY_FILE}.log
+     echo $NOW $* " ">>/var/tmp/consoleSys.log
 }
 
 if [ -z $CURRENT_HASHRATE ] ; then
@@ -21,29 +19,30 @@ if [ -z $CURRENT_HASHRATE ] ; then
 fi
 
 if [ -z $HOWMANY ] ; then
+    # right after a reboot, there's no record
     HOWMANY=0
 fi
+HOWMANY=$[ $HOWMANY + 1 ]
 
 if [ $CURRENT_HASHRATE -le $MINHASHRATE ] ; then
     if [ ! -e $HOWMANY_FILE ] ; then
-        echo 1 > $HOWMANY_FILE   
+        echo $HOWMANY > $HOWMANY_FILE   
     else
         if [ ! -z $HOWMANY ] && [ $HOWMANY -ge $TOOMANY ] ; then
-     	    rm $HOWMANY 	
-	    logmsg hashRate at or below $MINHASHRATE for $HOWMANY minutes. rebooted on 
-	    sync; sync; sync; sync
+     	    rm $HOWMANY
+	    logmsg hashRate at or below $MINHASHRATE for $HOWMANY minutes. rebooted because we exceeded $TOOMANY.
 	    rm $HOWMANY_FILE
+	    sync; sync; sync; sync
 	    sudo reboot     
+	    exit 0
 	else
-	    echo $[ $HOWMANY + 1 ] >$HOWMANY_FILE
+	    echo $HOWMANY >$HOWMANY_FILE
 	fi   
     fi   
-    logmsg hashRate at or below $MINHASHRATE for $( cat $HOWMANY_FILE ) minutes. 
+    logmsg hashRate at or below $MINHASHRATE for $HOWMANY minutes. 
 else     
-    if [ -z $HOWMANY ] ; then 
-        logmsg hashRate back to $CURRENT_HASHRATE after unknown time. 
-    elif [ $HOWMANY -ne 0 ] ; then
-        logmsg hashRate back to $CURRENT_HASHRATE after $( cat $HOWMANY_FILE ) minutes. 
+    if [ $HOWMANY -ne 1 ] ; then
+        logmsg hashRate back to $CURRENT_HASHRATE after $HOWMANY minutes.
     fi     
     echo 0 > $HOWMANY_FILE 
 fi
