d988a0760796182ef7754dba260383f7d2406923
Barry A Dobyns <bdobyns@gmail.com>
2021-11-11 06:14:09 +0100

    use /root/utils/stats_rig.sh instead of lolminer specific call to api

--- reboot-on-fail.sh
+++ reboot-on-fail.sh
@@ -3,8 +3,6 @@ cd ~
 TOOMANY=10 # it takes a few minutes to get everything stable after a reboot.
 MINHASHRATE=26 # at least one GPU running, even if not LHR mode
 HOWMANY_FILE=~/badhashes  
-CURRENT_HASHRATE=`curl -s localhost:4444 2>/dev/null | jq .Session.Performance_Summary | cut -f 1 -d .`
-HOWMANY=`cat $HOWMANY_FILE`  
 
 logmsg() {
      NOW=$( date '+%Y-%m-%d %H:%M:%S' )
@@ -12,14 +10,28 @@ logmsg() {
      echo $NOW $* " ">>/var/tmp/consoleSys.log
 }
 
-if [ -z $CURRENT_HASHRATE ] ; then
-    # starting up, or not running
-    logmsg hashRate is null 
-    CURRENT_HASHRATE=0
+if [ ! -e /var/tmp/rigStarted.run ] ; then
+    # the rig is not running properly.  
+    # don't reboot and get into a loop
+    rm $HOWMANY_FILE
+    exit
 fi
 
+# lolminer only for .Session.Performance_Summary
+# CURRENT_HASHRATE=`curl -s localhost:4444 2>/dev/null | jq .Session.Performance_Summary | cut -f 1 -d .`
+CURRENT_HASH_SEC=`/root/utils/stats_rig.sh | jq -r .hash | cut -f 1 -d . `
+if [ -z $CURRENT_HASH_SEC ] ; then
+    # starting up, or not running
+    logmsg 'hashRate is null (can not find miner?)'
+    CURRENT_HASHRATE=0
+else
+    # stats_rig.sh reports Hash/s not MH/s
+    CURRENT_HASHRATE=$[ $CURRENT_HASH_SEC / 1000 / 1000 ]
+fi
+
+HOWMANY=`cat $HOWMANY_FILE`  
 if [ -z $HOWMANY ] ; then
-    # right after a reboot, there's no record
+    # likely right after a reboot, there's no record
     HOWMANY=0
 fi
 HOWMANY=$[ $HOWMANY + 1 ]
@@ -29,8 +41,7 @@ if [ $CURRENT_HASHRATE -le $MINHASHRATE ] ; then
         echo $HOWMANY > $HOWMANY_FILE   
     else
         if [ ! -z $HOWMANY ] && [ $HOWMANY -ge $TOOMANY ] ; then
-     	    rm $HOWMANY
-	    logmsg hashRate at or below $MINHASHRATE for $HOWMANY minutes. rebooted because we exceeded $TOOMANY.
+	    logmsg hashRate at or below $MINHASHRATE for $HOWMANY minutes. rebooted because we exceeded $TOOMANY minutes.
 	    rm $HOWMANY_FILE
 	    sync; sync; sync; sync
 	    sudo reboot     
